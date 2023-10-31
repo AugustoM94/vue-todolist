e39059d192edf85e664f69d084a25d80656d1344
AugustoM94 <email@github.com>
2023-10-31 20:37:25 +0100

    added template

--- js/script.js
+++ js/script.js
@@ -5,7 +5,23 @@ createApp({
     data () {
         return {
             
-
+            todos: [
+                {
+                    id: 1,
+                    text: 'Fare i compiti',
+                    done: false
+                },
+                {
+                    id: 2,
+                    text: 'Fare la spesa',
+                    done: true
+                },
+                {
+                    id: 3,
+                    text: 'Fare il bucato',
+                    done: false,
+                }
+            ],
  
               
         };
