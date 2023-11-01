69ca59fe00d72c63fd563778996835fa0a922e0d
AugustoM94 <email@github.com>
2023-11-01 13:01:04 +0100

    Project completed

--- js/script.js
+++ js/script.js
@@ -1,34 +1,62 @@
-const { createApp } = Vue
-
+const { createApp } = Vue;
 
 createApp({
-    data () {
-        return {
-            
-            todos: [
-                {
-                    id: 1,
-                    text: 'Fare i compiti',
-                    done: false
-                },
-                {
-                    id: 2,
-                    text: 'Fare la spesa',
-                    done: true
-                },
-                {
-                    id: 3,
-                    text: 'Fare il bucato',
-                    done: false,
-                }
-            ],
- 
-              
-        };
+  data() {
+    return {
+     
+      todos: [
+        {
+          id: 1,
+          text: "Svegliarsi al mattino ",
+          done: false,
+        },
+        {
+          id: 2,
+          text: "Fare la colazione",
+          done: true,
+        },
+        {
+          id: 3,
+          text: "Fare lezione",
+          done: false,
+        },
+        {
+            id: 4,
+            text: "Pranzare",
+            done: true,
+          },
+          {
+            id: 5,
+            text: "Fare esercizio",
+            done: false,
+          },
+      ],
+      todoText: "",
+      lastId: 5,
+    };
+  },
+  methods: {
+    addTodo() {
+        this.lastId++;
+        const newTask = {
+          id: this.lastId,
+          text: this.todoText,
+          done:false
+        }
+        this.todos.push(newTask);
+        this.todoText = ''
     },
-
-
-    methods: {
-       
-    }
- }).mount('#app');
+    removeTodo(id) {
+      const index = this.todos.findIndex((todo) => todo.id === id);
+      if (index !== -1) {
+        this.todos.splice(index, 1);
+      }
+    },
+    doneTik(id) {
+        const todo = this.todos.find((t) => t.id === id);
+        if (todo) {
+          todo.done = !todo.done
+        }
+      },
+  },
+}).mount('#app');
